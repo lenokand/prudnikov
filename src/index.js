9da3f1ab05f22fe319bfc42f1444d3d212d9cc66
unknown <shtanko1991@gmail.com>
2021-05-05 14:05:57 +0300

    aaa

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import 'bootstrap';
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 import "./js/libs";
 // import './style/style.scss'
+
+
+const burger = document.getElementById('burger-menu')
+const shadow = document.getElementById('shadow')
+const menu = document.getElementById('main-menu')
+
+
+
+
+const toggleMenu = () => {
+    burger.classList.toggle('open')
+    menu.classList.toggle('open')
+    shadow.classList.toggle('open')
+    services.classList.toggle('open')
+}
+burger.addEventListener('click', toggleMenu)
+shadow.addEventListener('click', toggleMenu)
